2bbe1de012c9f1ff571f9ac215e81bb681706883
lesly <lkpsmx@gmail.com>
2021-07-02 16:17:39 -0500

    chore:highscore and button functionality again

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const secretNumber = Math.floor(Math.random() * 20) + 1;
+let secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -17,6 +18,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     // when guess is to high
   } else if (guess > secretNumber) {
@@ -47,4 +53,18 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector("body").style.backgroundColor = "rgb(206, 56, 56)";
     }
   }
+});
+
+// restart values
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
